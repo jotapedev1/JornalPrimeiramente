2944fd77562795f321b8f69a988a4ac488c847e5
JoaoSantos5195 <jotape5195@gmail.com>
2025-10-02 18:02:08 -0300

    Inicializando SpringBoot com Java21

---
+++ src/main/java/com/CJSantos/Jornal_Primeiramente/JornalPrimeiramenteApplication.java
@@ -0,0 +1,13 @@
+package com.CJSantos.Jornal_Primeiramente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JornalPrimeiramenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JornalPrimeiramenteApplication.class, args);
+	}
+
+}
