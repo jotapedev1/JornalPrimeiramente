6718193d47d529e5c4cf318a5dc51be5b6c6abf9
ccqsantos <caua0.cicarone@gmail.com>
2025-10-09 20:02:00 -0300

    add estrutura de config bd, add ex de model

---
+++ src/main/java/com/CJSantos/Jornal_Primeiramente/model/integranteModel.java
@@ -0,0 +1,33 @@
+package com.CJSantos.Jornal_Primeiramente.model;
+
+import jakarta.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "integrante")
+public class integranteModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_integrante", columnDefinition = "CHAR(36)")
+    private UUID idIntegrante;
+
+    @Column(nullable = false, length = 50)
+    private String nome;
+
+    @Column(nullable = false, length = 10)
+    private String turma;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String email;
+
+    @Lob
+    private byte[] foto;
+
+    @Column(nullable = false, length = 100)
+    private String hash;
+
+    // Construtores, Getters e Setters
+    public integranteModel() {}
+
+    // Getters e Setters...
+}
