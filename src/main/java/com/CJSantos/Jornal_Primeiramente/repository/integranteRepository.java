7f9cbb0c472074f0c03c0c9f93affc5d917f2018
ccqsantos <caua0.cicarone@gmail.com>
2025-10-10 18:57:59 -0300

    add flywaydb dependecy, add security name and password

---
+++ src/main/java/com/CJSantos/Jornal_Primeiramente/repository/integranteRepository.java
@@ -0,0 +1,6 @@
+package com.CJSantos.Jornal_Primeiramente.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.CJSantos.Jornal_Primeiramente.model.integranteModel;
+
+public interface integranteRepository extends JpaRepository<integranteModel, Long> {}
